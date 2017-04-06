fcbc48ed7bbb492e23ef0404de3550d75e8324ab
Kristin <krischleif@gmail.com>
2017-04-06 15:18:45 -0500

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react';
 import './Hero.css'
 
-const Hero = ({title, health, attack, exhausted}) => (
+const Hero = ({title, health, attack, exhausted, frozen}) => (
   <div className={`
       Hero
       ${exhausted ? '' : 'playable'}
+      Hero
+      ${frozen ? '' : 'playable'}
   `}>
     <div className="Hero-Title">{ title }</div>
     <div className="Hero-Health token">{ health }</div>
@@ -17,10 +19,12 @@ Hero.propTypes = {
   health: PropTypes.number.isRequired,
   attack: PropTypes.number.isRequired,
   exhausted: PropTypes.bool,
+  frozen: PropTypes.bool,
 }
 
 Hero.defaultProps = {
   exhausted: true,
+  frozen: true,
 }
 
 export default Hero
